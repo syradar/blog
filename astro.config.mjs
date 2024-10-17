70b53ea0f6a3d015ab9f4747d84a867425711aad
David Malmström <syntex@outlook.com>
2024-10-17 23:12:45 +0200

    minimal poc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://syradar.github.io",
+	base: "/blog",
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: "Syradar Blog",
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: "https://github.com/syradar/blog",
 			},
 			sidebar: [
 				{
-					label: 'Guides',
+					label: "Guides",
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: "Example Guide", slug: "guides/example" },
 					],
 				},
 				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: "Reference",
+					autogenerate: { directory: "reference" },
 				},
 			],
 		}),
